769959fe9e15a7f7909211a5975bb50675d6f90a
Precious <Preshomondi@gmail.com>
2022-08-20 05:49:26 +0300

    commit tree 0cb841ef46ab6b62da6f7e1d279bba742e695004

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def badge_maker str
+    "Hello, my name is #{str}."
+end
+
+def batch_badge_creator array
+    array.map do |arr|
+        "Hello, my name is #{arr}."
+    end
+end
+
+def assign_rooms array
+    array.map.with_index(1) do |arr, num|
+        "Hello, #{arr}! You'll be assigned to room #{num}!"
+    end
+end
+
+def printer(array)
+    batch_badge_creator(array).each do |badge|
+        puts badge
+    end
+    assign_rooms(array).each do |assignment|
+        puts assignment
+    end
+
+end
